197c571f2911ed55f1a2a9bc1ebc4b8f96c4060d
smurphy2230 <smurphy2230@gmail.com>
2020-04-23 19:33:18 -0400

    first two requirements done

---
+++ projectJS.js
@@ -0,0 +1,37 @@
+function main() {
+    // Add name and course elements at the top of the webpage
+    const body = document.body;
+    body.insertBefore(nameElement(), body.childNodes[0]);
+    body.insertBefore(courseElement(), body.childNodes[1]);
+}
+
+function nameElement() {
+    // write my name with the following formatting: H1 header, red, Tahoma, centered
+    
+    let nameElem = document.createElement("h1");
+    nameElem.style.color = "red";
+    nameElem.style.fontFamily = "Tahoma";
+    nameElem.style.textAlign = "center";
+
+    let nameText = document.createTextNode("Steven Murphy");
+    nameElem.appendChild(nameText);
+
+    return nameElem;
+}
+
+function courseElement() {
+    // write my course info with the following formatting: H2 header, red, Garamond, italic, centered
+
+    let courseElem = document.createElement("h2");
+    courseElem.style.color = "red";
+    courseElem.style.fontFamily = "Garamond";
+    courseElem.style.fontStyle = "italic";
+    courseElem.style.textAlign = "center";
+
+    let courseText = document.createTextNode("WEB-115 Section-0001");
+    courseElem.appendChild(courseText);
+
+    return courseElem;
+}
+
+main()
