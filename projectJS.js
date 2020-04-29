c8e026c9a6f33b3be1a92243fc40123a7e0657ac
smurphy2230 <smurphy2230@gmail.com>
2020-04-29 10:23:43 -0400

    final project complete!

--- projectJS.js
+++ projectJS.js
@@ -38,15 +38,15 @@ function courseElement() {
     return courseElem;
 }
 
+// function to validate email before resume is created
 function validateEmail() {
+
     const regEx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if (regEx.test(document.getElementById("email").value) == true) {
-        //console.log("valid email");
         return true;
     } else {
-        alert("Email address is not valid");
+        // alert("Email address is not valid");
         return false;
-        
     }
 }
 
@@ -58,7 +58,7 @@ function createNewResume() {
         return;
     }
 
-    const newResume = createNewPage(600, 900).document;
+    const newResume = createNewPage(400, 600).document;
 
     newResume.write("<html>");
     newResume.write("<head>");
@@ -85,22 +85,27 @@ function createNewPage(height, width) {
 function writeNewStyle(newResume) {
     const fontSize = 12;
     const fontFamily = "Courier, Monaco, monospace";
-    // const leftMargin = 20;
-    const paddingBottom = "10px";
+    const topMargin = 10;
+    const leftMargin = 20;
+    const marginBottom = 10;
 
     let styleString = "<style>";
     styleString += "body { ";
     styleString += ("font-size:" + fontSize + "pt;")
     styleString += ("font-family:" + fontFamily + ";")
+    styleString += (" margin-left:" + leftMargin + "px" + ";");
+    styleString += (" margin-top:" + topMargin + "px");
     styleString += " } ";
-    styleString += "#left { ";
-    styleString += ("float: left; width: 15%");
-    // styleString += (" margin-bottom:" + bottomMargin + ";");
+    styleString += ".column {";
+    styleString += "float: left;";
     styleString += " } ";
-    styleString += "#right { ";
-    styleString += ("float: right; width: 30%")
-    // styleString += (" margin-left:" + leftMargin + "%");
-    styleString += (" padding-bottom:" + paddingBottom + ";");
+    styleString += ".left { ";
+    styleString += ("width: 25% ;");
+    styleString += (" margin-bottom:" + marginBottom + "px");
+    styleString += " } ";
+    styleString += ".right { ";
+    styleString += ("width: 75% ;")
+    styleString += (" margin-bottom:" + marginBottom + "px");
     styleString += " } ";
     styleString += ".clear {";
     styleString += "clear: both;";
@@ -112,27 +117,32 @@ function writeNewStyle(newResume) {
 
 // Write new resume user contact info
 function writeNewPersonalData(newResume) {
+
     const inputForm = document.getElementById("resumeForm");
 
     let name = inputForm.name.value;
     newResume.write("<h4>" + name.toUpperCase() + "</h4>");
 
     let address = inputForm.streetAddress.value;
-    newResume.write("<p>" + address + "</p>");
+    newResume.write("<p>" + address.toUpperCase() + "</p>");
 
     let city = inputForm.city.value;
     let state = inputForm.state.value;
     let zipCode = inputForm.zipCode.value;
-    newResume.write("<p>" + city + ", " + state.toUpperCase() + " " + zipCode + "</p>");
+    newResume.write("<p>" + city.toUpperCase() + ", " + state.toUpperCase() + " " + zipCode + "</p>");
 
     let phone = inputForm.phone.value;
     newResume.write("<p>" + phone + "</p>");
 
     let email = inputForm.email.value;
     newResume.write("<p>" + email + "</p>");
+
+    newResume.write("<hr>")
 }
 
+// create new resume body section
 function writeNewBody(newResume) {
+
     const inputForm = document.getElementById("resumeForm");
 
     writeBodySection(newResume, "<b>Career Objectives</b>", formatTextarea(inputForm.careerObj.value));
@@ -140,10 +150,11 @@ function writeNewBody(newResume) {
     writeBodySection(newResume, "<b>Education</b>", formatTextarea(inputForm.educationData.value));
     writeBodySection(newResume, "<b>Previous Experience</b>", "");
     writeNewEmpSection(newResume);
-    writeBodySection(newResume, "<b>Background References</b>", formatTextarea(inputForm.businessRef.value));
-    
+    writeBodySection(newResume, "<b>Character References</b>", "Upon Request");
+    writeBodySection(newResume, "<b>Background References</b>", formatTextarea(inputForm.businessRef.value));    
 }
 
+// write employment section
 function writeNewEmpSection(newResume) {
 
     let prevEmp = document.getElementsByName("prevEmp");
@@ -152,6 +163,7 @@ function writeNewEmpSection(newResume) {
 
     for (let index = 0; index < prevEmp.length; index++) {
 
+        // if no start date don't include
         if (startDates[index].value) {
 
             let dateString = getDateString(startDates[index].value, endDates[index].value);
@@ -160,13 +172,15 @@ function writeNewEmpSection(newResume) {
     }
 }
 
+// add div classes to body elements for styling
 function writeBodySection(newResume, left, right) {
 
-    newResume.write("<div id=\"left\">" + left + "</div>");
-    newResume.write("<div id=\"right\">" + right + "</div>");
+    newResume.write("<div class=\"column left\">" + left + "</div>");
+    newResume.write("<div class=\"column right\">" + right + "</div>");
     newResume.write("<div class=\"clear\"></div>");
 }
 
+// make sure new text area formatting mirrors input form
 function formatTextarea(value) {
 
     let lines = value.split("\n");
@@ -179,6 +193,7 @@ function formatTextarea(value) {
     return string;
 }
 
+// format dates to MON. YYYY
 function getDateString(start, end) {
 
     let startMonth = start.charAt(5) + start.charAt(6);
